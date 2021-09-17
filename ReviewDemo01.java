9558a1fdbedc080af6b0f97f6752e3e40f3a755e
DoHyeonStudy <kdh98241@gmail.com>
2021-09-17 21:52:47 +0900

    week02 v0.5

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -1,4 +1,4 @@
-//v0.3
+//v0.5
 import java.util.Scanner;
 
 public class ReviewDemo01
@@ -9,9 +9,28 @@ public class ReviewDemo01
         int number = sc.nextInt();
         boolean isPrime = true;
 
+        /*
         for(int k=2; k<number; k++)
         {
-            if(number % k == 0) isPrime = false;
+            if(number % k == 0)
+            {
+                isPrime = false;
+                break;
+            }
+            System.out.println(k);
+        }
+         */
+
+        int k=2;
+        while(k<number)
+        {
+            if(number % k == 0)
+            {
+                isPrime = false;
+                break;
+            }
+            //System.out.println(k);
+            k++;
         }
 
         if(isPrime) System.out.println(number + "은(는) 소수!");
